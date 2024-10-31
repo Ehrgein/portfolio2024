eb67111042926c02eda21851acdf8bf76b5a5bba
Alek <ehrgein@gmail.com>
2024-10-30 23:54:00 -0300

    eslint err

--- src/app/Pages/WhiteLanding.tsx
+++ src/app/Pages/WhiteLanding.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { useRef } from "react";
-import WhiteNavbar from "../Components/layout/WhiteNavbar";
+// import WhiteNavbar from "../Components/layout/WhiteNavbar";
 import PresentationParagraph from "../Components/animations/PresentationParagraph";
 import FooterWhite from "../Components/layout/FooterWhite";
 
