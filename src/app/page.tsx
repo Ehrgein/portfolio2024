a2d196dfb20d457afc824f5b87d070482950bf5e
Alek <ehrgein@gmail.com>
2024-11-05 23:06:40 -0300

    CSS Fixes, mobile layou for testing purposes.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -74,68 +74,71 @@ export default function Home() {
       {isLoading ? (
         <IntroAnimation isLoading={isLoading} setIsLoading={setIsLoading} />
       ) : (
-        <main className="hidden md:block">
-          <div>
-            <motion.main
-              key="main-content"
-              ref={containerRef}
-              className={`h-[200vh] py-2 relative`}
-            >
-              <WhiteNavbar navBarColor={navBarColor} />
-              <motion.div
-                style={{ opacity }}
-                ref={mainRef}
-                className={`sticky top-0 justify-center w-full flex flex-col flex-grow`}
+        <>
+          <main className="hidden md:block">
+            <div>
+              <motion.main
+                key="main-content"
+                ref={containerRef}
+                className={`h-[200vh] py-2 relative`}
               >
-                <section className="h-screen w-full flex flex-col px-4">
-                  <PresentationParagraph />
+                <WhiteNavbar navBarColor={navBarColor} />
+                <motion.div
+                  style={{ opacity }}
+                  ref={mainRef}
+                  className={`sticky top-0 justify-center w-full flex flex-col flex-grow`}
+                >
+                  <section className="h-screen w-full flex flex-col px-4">
+                    <PresentationParagraph />
+                  </section>
+                </motion.div>
+                <section
+                  ref={projectsectionRef}
+                  className="relative w-full bg-[#161616] px-32 pt-20 pb-32"
+                >
+                  <SectionTwo />
                 </section>
-              </motion.div>
-              <section
-                ref={projectsectionRef}
-                className="relative w-full bg-[#161616] px-32 pt-20 pb-32"
-              >
-                <SectionTwo />
-              </section>
-              <section className="py-20 h-screen">
-                <p>hello!</p>
-              </section>
-            </motion.main>
+                <section className="py-20 h-screen">
+                  <p>hello!</p>
+                </section>
+              </motion.main>
+            </div>
+          </main>
+          <div className="noise-bg"></div>
+          {/* mobile layout */}
+          <div className="md:hidden">
+            <main className="mb-[-100svh] overflow-x-clip">
+              <div>
+                <motion.main
+                  key="main-content"
+                  ref={containerRef}
+                  className={`h-[200vh] py-2 relative`}
+                >
+                  <WhiteNavbar navBarColor={navBarColor} />
+                  <motion.div
+                    style={{ opacity }}
+                    ref={mainRef}
+                    className={`sticky top-0 h-[100svh] items-end justify-center w-full flex flex-col flex-grow`}
+                  >
+                    <section className="w-full flex flex-col px-4">
+                      <PresentationParagraph />
+                    </section>
+                  </motion.div>
+                  <section
+                    ref={projectsectionRef}
+                    className="relative w-full bg-[#161616] px-32 pt-20 pb-32"
+                  >
+                    <SectionTwo />
+                  </section>
+                  <section className="py-20 h-screen">
+                    <p>hello!</p>
+                  </section>
+                </motion.main>
+              </div>
+            </main>
           </div>
-        </main>
+        </>
       )}
-      <div className="noise-bg"></div>
-      <div className="md:hidden">
-        <main className="mb-[-100svh] overflow-x-clip">
-          <div>
-            <motion.main
-              key="main-content"
-              ref={containerRef}
-              className={`h-[200vh] py-2 relative`}
-            >
-              <WhiteNavbar navBarColor={navBarColor} />
-              <motion.div
-                style={{ opacity }}
-                ref={mainRef}
-                className={`sticky top-0 h-[100svh] items-end justify-center w-full flex flex-col flex-grow`}
-              >
-                <section className="w-full flex flex-col px-4">
-                  <PresentationParagraph />
-                </section>
-              </motion.div>
-              <section
-                ref={projectsectionRef}
-                className="relative w-full bg-[#161616] px-32 pt-20 pb-32"
-              >
-                <SectionTwo />
-              </section>
-              <section className="py-20 h-screen">
-                <p>hello!</p>
-              </section>
-            </motion.main>
-          </div>
-        </main>
-      </div>
     </>
   );
 }
