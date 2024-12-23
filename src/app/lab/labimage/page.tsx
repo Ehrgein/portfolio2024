299d155bac3064da9c381023af5f13c7505d66b6
ehrgein <ehrgein@gmail.com>
2024-12-22 23:24:59 -0300

    eslint

--- src/app/lab/labimage/page.tsx
+++ src/app/lab/labimage/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import dynamic from "next/dynamic";
-import MagneticSVG from "@/app/Components/animations/MagneticSVG";
 
 const Scene = dynamic(() => import("@/app/Components/scene/Scene"), {
   ssr: false,
@@ -11,11 +10,9 @@ function LabImage() {
   return (
     <div className="h-screen">
       <Scene />
-      <MagneticSVG>
-        <div className="w-24 h-24">
-          <div className="w-24  bg-blue-500"></div>
-        </div>
-      </MagneticSVG>
+      <div className="w-24 h-24">
+        <div className="w-24  bg-blue-500"></div>
+      </div>
     </div>
   );
 }
