dbed317f351927715127b14cac2eafac78e30847
ehrgein <ehrgein@gmail.com>
2024-12-27 21:30:28 -0300

    1

--- src/app/Components/transitions/SwitchProject.tsx
+++ src/app/Components/transitions/SwitchProject.tsx
@@ -7,7 +7,7 @@ function SwitchProject() {
       initial={{ y: "100%" }}
       animate={{ y: "0%" }}
       transition={{ duration: 1, ease: [0.32, 0, 0.2, 1] }}
-      className="inset-0 absolute bg-[#EDE9DE] z-[99999999999999999999999] h-full"
+      className="inset-0 absolute bg-[#EDE9DE] z-[9999999] h-full"
     ></motion.div>
   );
 }
