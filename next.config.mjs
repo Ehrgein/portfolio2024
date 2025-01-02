dbeb5d828e37fb47f0ba0a276f926dcc09278991
ehrgein <ehrgein@gmail.com>
2025-01-02 11:44:54 -0300

    removed bundle analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,4 @@
 /** @type {import('next').NextConfig} */
-
-import withBundleAnalyzer from "@next/bundle-analyzer";
-
-const bundleAnalyzer = withBundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
 const nextConfig = {};
 
-export default bundleAnalyzer(nextConfig);
+export default nextConfig;
